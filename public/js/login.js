1678dcd465b53ac06d93861925c6cf0c318176a5
Richo Ha <richard.dqh@gmail.com>
2021-06-27 12:38:21 +0800

    completed login html and styling

--- public/js/login.js
+++ public/js/login.js
@@ -1,13 +1,11 @@
+// Login Page Javascript
 const loginFormHandler = async (event) => {
-  // TODO: Add a comment describing the functionality of this statement
   event.preventDefault();
 
-  // TODO: Add a comment describing the functionality of these expressions
   const email = document.querySelector('#email-login').value.trim();
   const password = document.querySelector('#password-login').value.trim();
 
   if (email && password) {
-    // TODO: Add a comment describing the functionality of this expression
     const response = await fetch('/api/users/login', {
       method: 'POST',
       body: JSON.stringify({ email, password }),
@@ -15,7 +13,7 @@ const loginFormHandler = async (event) => {
     });
 
     if (response.ok) {
-      document.location.replace('/');
+      document.location.replace('/profile');
     } else {
       alert('Failed to log in');
     }
@@ -25,3 +23,61 @@ const loginFormHandler = async (event) => {
 document
   .querySelector('.login-form')
   .addEventListener('submit', loginFormHandler);
+
+
+
+// Sign Up Page Javascript
+
+const signupFormHandler = async (event) => {
+  event.preventDefault();
+
+  const name = document.querySelector('#username-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+
+  if (name && email && password) {
+    const response = await fetch('/api/users/signup', {
+      method: 'POST',
+      body: JSON.stringify({ name, email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/profile');
+    } else {
+      alert('Failed to sign up.');
+    }
+  }
+};
+
+document
+  .querySelector('.signup-form')
+  .addEventListener('submit', signupFormHandler);
+
+
+// Toggling between cards Javascript
+
+// Getting Cards 
+var loginCard = document.getElementById('loginCard');
+var signupCard = document.getElementById('signupCard');
+
+// Get the buttons
+var signupPageRequest = document.querySelector('#signup');
+var loginPageRequest = document.querySelector('#login');
+
+// Note cards have hidden attribute.
+// This is to switch login on
+loginCard.classList.toggle("hidden");
+
+// Getting when click, hide all except requested card
+document.querySelector('#signup')
+  .addEventListener("click", () => {
+    loginCard.classList.toggle("hidden");
+    signupCard.classList.toggle("hidden");
+  });
+
+document.querySelector('#login')
+  .addEventListener("click", () => {
+    signupCard.classList.toggle("hidden");
+    loginCard.classList.toggle("hidden");
+  });
